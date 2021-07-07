6c6c51126e5174bfd20a4fab8d40a8937fa764ae
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-08 00:03:40 +0530

    update the property borderBottomWidth for the items attribute inside the const styles

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
   },
 
   items: {
-    borderRightWidth: 2,
+    borderBottomWidth: 2,
     borderBottomColor: 'red',
     flex: 0.25,
     flexDirection: 'row',
